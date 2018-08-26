46fbffab8cd4ec7dfb485ec2ba1f6148300f652d
Ola Bini <ola@olabini.se>
2018-08-26 17:29:54 -0500

    Some annoying things for the travis script

--- travis_before_script.sh
+++ travis_before_script.sh
@@ -13,38 +13,33 @@ LIBGOLDILOCKS_DIR=.deps/libgoldilocks
 export PREFIX=/tmp/prefix
 
 if [[ -f $GPG_ERROR_DIR/src/.libs/libgpg-error.so ]]; then
-    pushd $GPG_ERROR_DIR && make install && popd
+    (cd $GPG_ERROR_DIR && make install)
 else
     curl https://www.gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.26.tar.bz2 | tar xjf - -C .deps
-    pushd $GPG_ERROR_DIR && ./configure --prefix=$PREFIX && make && make install && popd
+    (cd $GPG_ERROR_DIR && ./configure --prefix=$PREFIX && make && make install)
 fi
 
-if [[ -f $LIBGCRYPT_DIR/src/.libs/libgcrypt.so ]]; then
-    pushd $LIBGCRYPT_DIR && make install && popd
-else
-    curl https://www.gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-1.8.1.tar.bz2 | tar xjf - -C .deps
-    pushd $LIBGCRYPT_DIR && ./configure --with-libgpg-error-prefix=$PREFIX --prefix=$PREFIX && make && make install && popd
-fi
+curl https://www.gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-1.8.1.tar.bz2 | tar xjf - -C .deps
+(cd $LIBGCRYPT_DIR && ./configure --with-libgpg-error-prefix=$PREFIX --prefix=$PREFIX && make && make install)
 
 if [[ -f $LIBSODIUM_DIR/src/libsodium/.libs/libsodium.so ]]; then
-    pushd $LIBSODIUM_DIR && make install && popd
+    (cd $LIBSODIUM_DIR && make install)
 else
     curl https://download.libsodium.org/libsodium/releases/LATEST.tar.gz | tar xzf - -C .deps
-    pushd $LIBSODIUM_DIR && ./autogen.sh && ./configure --prefix=$PREFIX && make && make install && popd
+    (cd $LIBSODIUM_DIR && ./autogen.sh && ./configure --prefix=$PREFIX && make && make install)
 fi
 
-if [[ -f $LIBOTR_DIR/src/.libs/libotr.so ]]; then
-    pushd $LIBOTR_DIR && make install && popd
-else
+if [[ ! -e $LIBOTR_DIR ]]; then
     git clone --depth=1 https://bugs.otr.im/lib/libotr.git $LIBOTR_DIR
-    pushd $LIBOTR_DIR && ./bootstrap && ./configure --prefix=$PREFIX && make && make install && popd
 fi
+(cd $LIBOTR_DIR && ./bootstrap && ./configure && make && sudo make install)
 
 if [[ -f $LIBGOLDILOCKS_DIR/src/.libs/libgoldilocks.so ]]; then
-    pushd $LIBGOLDILOCKS_DIR && make install && popd
+    (cd $LIBGOLDILOCKS_DIR && sudo make install)
 else
+    rm -rf $LIBGOLDILOCKS_DIR
     git clone --depth=1 https://github.com/otrv4/libgoldilocks $LIBGOLDILOCKS_DIR
-    pushd $LIBGOLDILOCKS_DIR && ./autogen.sh && ./configure --prefix=$PREFIX && make && make install && popd
+    (cd $LIBGOLDILOCKS_DIR && ./autogen.sh && ./configure && make && sudo make install)
 fi
 
 if [[ ! -f .deps/pidgin.tar.bz2 ]]; then
